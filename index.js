1f4484b7dca6e707118257cb5ddacb9dce07c5d0
Roman2864 <69533404+Roman2864@users.noreply.github.com>
2022-04-13 19:59:55 +0300

    Create index.js
    Test

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# RS" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Roman2864/RS.git
+git push -u origin main
